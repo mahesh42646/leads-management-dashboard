4d029cfcf66db826142292494462dc0034d48202
Mahesh Darkunde <maheshdarkunde2@gmail.com>
2026-03-26 11:10:42 +0530

    Remove redundant output property from config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    eslint: {
-          ignoreDuringBuilds: true,
-    },
+      reactStrictMode: true,
+      eslint: {
+              ignoreDuringBuilds: true,
+      },
 }
 
 module.exports = nextConfig
